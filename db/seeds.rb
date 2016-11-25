4cdeff7ccf5fb69661a0d325e26039ee9a94c702
chenlong1 <chenlong1@liandisys.com.cn>
2016-11-25 17:54:37 +0800

    11/25 晚上

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create(title:'SUNSTAR Ora2 キレイをまいにち　ドロピカルマンゴー',
+               description:
+                   %{
+        <p>
+            惊爆价，只要28，薄荷味漱口水带回家。made in china
+        </p>
+    },
+               image_url: 'ora2.jpg',
+               price:27.99
+)
+Product.create(title:'witcher3 昆特3',
+              description:
+                   %{
+        <p>
+            2015年度游戏，演绎了主角杰罗特和女儿，人妻，小三，女学生，女爵..的离奇故事
+            更有昆特牌局等待大家
+        </p>
+    },
+              image_url: 'witcher3.jpg',
+              price:148
+)
+Product.create(title:'Programming Ruby 2.3',
+              description:
+                   %{
+        <p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>
+    },
+              image_url: 'ruby.jpg',
+              price:68
+)
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
-
+#在controllerdir下的是integration集成测试
+#这里的testcase 是单元测试
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
